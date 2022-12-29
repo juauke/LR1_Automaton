e1940b2015753e8f6764972e4bf1a442f9439495
Justin BABONNEAU <juauke@live.fr>
2022-12-29 08:55:51 +0100

    Added files that belong to v2 and removed files that do not

---
+++ autov2/include/auto_DOT.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h> 
+
+#include "auto.h"
+#include "auto_file.h"
+
+#ifndef _AUTO_DOT_H
+
+#define _AUTO_DOT_H 1
+
+void DOTaut(aut_t *paut) ;
+
+#endif
